1eb09cdf31fdca00432705115ceda9be7f589049
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-21 22:31:48 +0530

    import shared module inside user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [AuthModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
