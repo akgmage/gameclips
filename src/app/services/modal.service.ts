b4988e11353527cca90b86ba7eef8733614c96b6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-23 23:02:32 +0530

    add IModal interface and register method which pushed id and visible property to modals array

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private visible = false;
+  private modals: IModal[] = [];
   constructor() {}
 
   isModalOpen() {
-    return this.visible;
+    return true;
   }
 
   toggleModal() {
-    this.visible = !this.visible;
+    // this.visible = !this.visible;
+  }
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false,
+    });
+    console.log(this.modals);
   }
 }
