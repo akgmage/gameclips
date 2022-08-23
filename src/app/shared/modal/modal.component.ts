98362cafb3eb205858ee3e82b0a9cceeda4f304b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-23 23:19:44 +0530

    receive modalID from parent

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,11 +7,12 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = '';
   constructor(public modal: ModalService) {}
 
   ngOnInit(): void {}
 
   closeModal() {
-    this.modal.toggleModal();
+    this.modal.toggleModal(this.modalID);
   }
 }
