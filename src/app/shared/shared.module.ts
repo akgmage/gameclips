2683dee517ea314a868fa5e1cb8bceec08ac90d7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-24 23:17:54 +0530

    add tabs-container component inside shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, TabsContainerComponent],
   imports: [CommonModule],
   exports: [ModalComponent],
   providers: [ModalService],
