e0e44b457c97d769702f915def0b10527aff1eb8
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-02 22:27:15 +0530

    add register component inside user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 @NgModule({
-  declarations: [AuthModalComponent, LoginComponent],
+  declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
   imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent],
 })
