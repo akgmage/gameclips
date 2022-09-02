bdf6eeb69b7b1a92d48d3b167f635070927ed0c5
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-02 22:43:52 +0530

    add 6 form control

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({});
+  registerForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  });
 }
