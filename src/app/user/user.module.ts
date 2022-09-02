9924e33a72d329907ccb7d2c65dc39f0d793dd6c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-02 22:23:10 +0530

    add login component inside user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 @NgModule({
-  declarations: [AuthModalComponent],
+  declarations: [AuthModalComponent, LoginComponent],
   imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent],
 })
