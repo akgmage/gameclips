5762fe5b31abaff0d9c585956c9332b0d2ee963e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-03 17:31:19 +0530

    add validators for name

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl(''),
     age: new FormControl(''),
     password: new FormControl(''),
