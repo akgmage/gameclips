adabfc270bd1654c415b5e7a3593ddebfef07f9b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-04 17:42:34 +0530

    import reactive forms module in shared component, replace formcontrolname with formcontrol

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,21 @@ import { ModalService } from '../services/modal.service';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
   providers: [ModalService],
 })
 export class SharedModule {}
