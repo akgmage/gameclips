96573f6c90afa295ff17a1d869d651f435247af9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-14 22:54:59 +0530

    add firebase configuration for dev and production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCZ0pmCpRDuLo7-XDuWO2yc2m9mE66kdJU',
+    authDomain: 'clips-5df31.firebaseapp.com',
+    projectId: 'clips-5df31',
+    storageBucket: 'clips-5df31.appspot.com',
+    appId: '1:101933579844:web:6b5d2deaf542cc6abe8611',
+    measurementId: 'G-N7TKPRT760',
+  },
 };
