6b99e8310e28b0eff1ffd44294e5ec6f742b9508
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-17 12:42:05 +0530

    initialize firebase before angular

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+firebase.initializeApp(environment.firebase);
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
