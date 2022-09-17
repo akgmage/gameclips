b67858ea61d793bb4608e9dc7b8ed15e7eb98227
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-17 13:00:31 +0530

    initialize alert component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  inSubmission = false;
+  showAlert = false;
+  alertMsg = "Please wait! You're being logged in.";
+  alertColor = 'blue';
   credentials = {
     email: '',
     password: '',
   };
-  constructor() {}
+  constructor(private auth: AngularFireAuth) {}
 
   ngOnInit(): void {}
 
-  login() {
-    console.log(this.credentials);
+  async login() {
+    try {
+      this.inSubmission = true;
+      this.showAlert = true;
+      this.alertMsg = "Please wait! You're being logged in.";
+      this.alertColor = 'blue';
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (e) {}
+    this.alertMsg = 'Login successful';
+    this.alertColor = 'green';
   }
 }
