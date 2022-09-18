1809ce68e5bc87f9b85e6ef3de76fb86c8cf7fe9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-18 13:08:07 +0530

    validate unique email

--- src/app/user/validators/emailtaken.ts
+++ src/app/user/validators/emailtaken.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-
+import {
+  AsyncValidator,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 @Injectable()
-export class Emailtaken {
-  constructor(private aith: AngularFireAuth) {}
+export class Emailtaken implements AsyncValidator {
+  constructor(private auth: AngularFireAuth) {}
+
+  validate(control: AbstractControl): Promise<ValidationErrors | null> {
+    this.auth
+      .fetchSignInMethodsForEmail(control.value)
+      .then((response) => (response.length ? { emailtaken: true } : null));
+  }
 }
