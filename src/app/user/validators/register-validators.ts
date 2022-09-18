961b91fa6da9b2cbc821b12077391f8d3968cf7a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-18 12:05:19 +0530

    add static method to check match for password and confirm password

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,2 +1,19 @@
+import { ValidationErrors, AbstractControl } from '@angular/forms';
 export class RegisterValidators {
+  static match(group: AbstractControl): ValidationErrors | null {
+    const control = group.get('password');
+    const matchingControl = group.get('confirmPassword');
+    if (!control || !matchingControl) {
+      return {
+        controlNotFound: false,
+      };
+    }
+    const error =
+      control.value === matchingControl.value
+        ? null
+        : {
+            noMatch: true,
+          };
+    return error;
+  }
 }
