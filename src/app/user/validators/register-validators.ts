a81b9faa8cbd81bf10a2fa68705f8bc198dd1552
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-18 12:21:27 +0530

    create dynamic validator using factory design pattern

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,19 +1,21 @@
-import { ValidationErrors, AbstractControl } from '@angular/forms';
+import { ValidationErrors, AbstractControl, ValidatorFn } from '@angular/forms';
 export class RegisterValidators {
-  static match(group: AbstractControl): ValidationErrors | null {
-    const control = group.get('password');
-    const matchingControl = group.get('confirmPassword');
-    if (!control || !matchingControl) {
-      return {
-        controlNotFound: false,
-      };
-    }
-    const error =
-      control.value === matchingControl.value
-        ? null
-        : {
-            noMatch: true,
-          };
-    return error;
+  static match(controlName: string, matchingControlName: string): ValidatorFn {
+    return (group: AbstractControl): ValidationErrors | null => {
+      const control = group.get(controlName);
+      const matchingControl = group.get(matchingControlName);
+      if (!control || !matchingControl) {
+        return {
+          controlNotFound: false,
+        };
+      }
+      const error =
+        control.value === matchingControl.value
+          ? null
+          : {
+              noMatch: true,
+            };
+      return error;
+    };
   }
 }
