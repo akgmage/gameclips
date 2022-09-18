f35aecda846e340ec442d09a8c5cc6858e9747e7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-18 13:01:02 +0530

    import AngularFireAuth and inject auth in constructor and add Injectable decorator on top of class

--- src/app/user/validators/emailtaken.ts
+++ src/app/user/validators/emailtaken.ts
@@ -1,2 +1,7 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable()
 export class Emailtaken {
+  constructor(private aith: AngularFireAuth) {}
 }
