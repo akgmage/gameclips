ba5571204ab653beb729e8e766f3ff61a4c1be2a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-26 23:27:08 +0530

    add Video module with routing flag on

---
+++ src/app/video/video.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VideoRoutingModule } from './video-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VideoRoutingModule
+  ]
+})
+export class VideoModule { }
