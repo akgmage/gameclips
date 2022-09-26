359f8e234ef5f798d3bfc0c9139f31cc892b7924
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-26 23:32:57 +0530

    register a route for manage component

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ManageComponent } from './manage/manage.component';
+const routes: Routes = [
+  {
+    path: 'manage',
+    component: ManageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class VideoRoutingModule { }
+export class VideoRoutingModule {}
