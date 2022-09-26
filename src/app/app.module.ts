71c9c8e78424cc53533f9edbabb4c4fd3a4b5638
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-26 23:30:05 +0530

    import video module in appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { VideoModule } from './video/video.module';
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent],
   imports: [
@@ -20,6 +21,7 @@ import { AboutComponent } from './about/about.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    VideoModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
