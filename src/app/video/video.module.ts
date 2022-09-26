b33ffd38be7884b21337dce152c959a12f15aba3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-26 23:31:05 +0530

    add manage component inside Video mopule

--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VideoRoutingModule } from './video-routing.module';
+import { ManageComponent } from './manage/manage.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ManageComponent
+  ],
   imports: [
     CommonModule,
     VideoRoutingModule
