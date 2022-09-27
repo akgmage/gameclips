2bd139abcbf5cad98b8bdca991a19182b2f5fde9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-27 22:51:03 +0530

    filter navigationEnd event

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,10 +5,10 @@ import {
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-import { delay, map } from 'rxjs/operators';
+import { delay, map, filter } from 'rxjs/operators';
 import IUser from '../models/user.model';
 import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 @Injectable({
   providedIn: 'root',
 })
@@ -25,6 +25,9 @@ export class AuthService {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(console.log);
   }
 
   public async createUser(userData: IUser) {
