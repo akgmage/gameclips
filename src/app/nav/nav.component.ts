d172d7ae6039bdd48c1018cab4990a59c7f4a521
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-27 22:33:57 +0530

    extract logout method

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,30 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(
-    public modal: ModalService,
-    public auth: AuthService,
-    public afAuth: AngularFireAuth,
-    private router: Router
-  ) {}
+  constructor(public modal: ModalService, public auth: AuthService) {}
 
   ngOnInit(): void {}
 
   openModal($event: Event) {
     $event.preventDefault();
     this.modal.toggleModal('auth');
-  }
-  async logout($event: Event) {
-    $event.preventDefault();
-    await this.afAuth.signOut();
-    await this.router.navigateByUrl('/');
   }
 }
