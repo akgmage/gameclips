7a73197bb7a4e2a262d10dcf0a7363cee2b65aeb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-28 22:49:29 +0530

    add clip component

---
+++ src/app/clip/clip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clip',
+  templateUrl: './clip.component.html',
+  styleUrls: ['./clip.component.css']
+})
+export class ClipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
