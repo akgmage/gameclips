5dfd4c9ea5844a45f26074ee62e2297501b2d5ad
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-28 22:38:21 +0530

    register a route with data

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
     data: {
       authOnly: true,
+    },
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
+    data: {
+      authONly: true,
     },
   },
 ];
