5404c7e30b59fdb2e80eaf26eb64ce8dc4e21f16
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-30 22:53:04 +0530

    redirect unauthorized users to home if they try to access auth routes

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
-import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+import {
+  AngularFireAuthGuard,
+  redirectUnauthorizedTo,
+} from '@angular/fire/compat/auth-guard';
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
     data: {
       authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
     },
     canActivate: [AngularFireAuthGuard],
   },
@@ -17,7 +22,9 @@ const routes: Routes = [
     component: UploadComponent,
     data: {
       authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
     },
+    canActivate: [AngularFireAuthGuard],
   },
   {
     path: 'manage-clips',
