26380692eb06b64ba5794ccdbb1ae5b556fa14f3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-30 23:08:52 +0530

    block event with directive

--- src/app/shared/directives/event-blocker.directive.ts
+++ src/app/shared/directives/event-blocker.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[app-event-blocker]',
 })
 export class EventBlockerDirective {
-  constructor() {}
+  @HostListener('drop', ['$event'])
+  @HostListener('dragover', ['$event'])
+  public handleevent(event: Event) {
+    event.preventDefault();
+    event.stopPropagation(); // prevent bubbling
+  }
 }
